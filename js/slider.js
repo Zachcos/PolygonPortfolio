c71e9c693008596eff077505ec07d6d4b734f24d
Zach Cossman <Zachcos@gmail.com>
2017-12-30 00:01:28 -0500

    Initial slide animation and intervals set

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,27 @@
 'use strict';
+
+$(function() {
+
+  // configuration
+  var width = 720;
+  var animationSpeed = 1000;
+  var pause = 3000;
+  var currentSlide = 1;
+
+  // cache the DOM
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide');
+
+  setInterval(function() {
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+      currentSlide++;
+
+      if (currentSlide === $slides.length) {
+        currentSlide = 1;
+        $slideContainer.css('margin-left', 0);
+      }
+    });
+  }, pause)
+
+});
